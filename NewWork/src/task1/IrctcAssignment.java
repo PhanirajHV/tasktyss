47d73718f5adbd354a194db4f0f4208411f5c209
roshan88 <roshankotharkar15@gmail.com>
2019-06-07 17:55:04 +0530

    new irctc

--- NewWork/src/task1/IrctcAssignment.java
+++ NewWork/src/task1/IrctcAssignment.java
@@ -1,5 +1,6 @@
 package task1;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -10,28 +11,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class IrctcAssignment {
+public class IrctcAssignment 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException 
+	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://irctc.co.in");
 		driver.findElement(By.xpath("//a[text()=' FLIGHTS ']")).click();
 		Set<String> set = driver.getWindowHandles();
-		Iterator<String> itrObj = set.iterator();
-		String parentSessionid = itrObj.next();
 		
-		WebDriver driver1=driver.switchTo().window(parentSessionid);
-		WebElement from = driver1.findElement(By.xpath("//input=[@id='stationFrom']"));
-		from.click();
-		from.sendKeys("banglore");
-		List<WebElement> fromlist = driver.findElements(By.xpath("//div[@class='ui-menu-item-wrapper']"));
+		ArrayList<String> al=new ArrayList<String>(set);
+		
+		driver.switchTo().window(al.get(1));
+		driver.findElement(By.xpath("//input[@name='From']")).click();
+		
+		List<WebElement> fromlist = driver.findElements(By.xpath("//ul[@class='ui-menu ui-widget ui-widget-content ui-autocomplete ui-front']/li"));
+		
 		int count = fromlist.size();
 		String expcity = "Bengaluru";
-		for (int i = 0; i <= count; i++) {
+		for (int i = 0; i <count; i++) 
+		{
+			//System.out.println(fromlist.get(i).getText());
 			String actcity = fromlist.get(i).getText();
-			if (expcity.contains(actcity)) {
+			
+			System.out.println(actcity);
+			if (expcity.contains(actcity)) 
+			{
 				fromlist.get(i).click();
 			}
 			
